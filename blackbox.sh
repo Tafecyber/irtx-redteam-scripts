abbeec79bd3489fbd57c59df7d9b860128cbbdf0
Tafecyber <ragnaroksnowdrop@gmail.com>
2026-06-01 13:39:28 +1000

    Update blackbox.sh

--- blackbox.sh
+++ blackbox.sh
@@ -184,13 +184,13 @@ if [[ "$SSH_CONFIRM" == "y" || "$SSH_CONFIRM" == "Y" ]]; then
   echo ""
   # Pre-accept host key
   ssh-keyscan -p "$SSH_PORT" "$TARGET" >> ~/.ssh/known_hosts 2>/dev/null
+  sleep 2
   # Connect with password
   sshpass -p "$PASSWORD" ssh \
     -o StrictHostKeyChecking=no \
     -o KexAlgorithms=+diffie-hellman-group1-sha1 \
     -o HostKeyAlgorithms=+ssh-rsa \
     "$USERNAME"@"$TARGET" -p "$SSH_PORT"
-fi
 
 # =============================================================
 # DONE
